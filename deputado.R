e70348a99ad3e7259782c295de3feeb154f1727d
vittorfp <vittorfpereira@gmail.com>
2017-11-02 16:19:51 -0200

    oi

--- deputado.R
+++ deputado.R
@@ -1,4 +1,11 @@
 library(XML)
 
-url <- 'http://www.camara.leg.br/internet/deputado/RelVotacoes.asp?nuLegislatura=55&nuMatricula=487&dtInicio=01/01/2017&dtFim=30/12/2017'
+url = 'http://www.camara.leg.br/internet/deputado/RelVotacoes.asp?nuLegislatura=55&nuMatricula=487&dtInicio=01/01/2017&dtFim=30/12/2017'
+pagina = readLines(url)
+pagina = htmlParse(pagina)  # Fala com o R para entender as linhas como um doc HTML
+pagina = xmlRoot(pagina)    # Tira as coisas que não estão dentro das TAGs HTML
 
+
+# Captura uma tag especifica da pagina
+txt = getNodeSet(pagina,"/html/head")
+txt = xmlSApply(txt,xmlValue)
